6d9ce7648a296bf58041c8881172658de8d7a596
Jihyun247 <wlguslove12345@gmail.com>
2024-07-24 22:46:24 +0900

    feature: network event logger

--- Projects/Core/APIClient/Sources/Plugin/EventLoggerDelegate.swift
+++ Projects/Core/APIClient/Sources/Plugin/EventLoggerDelegate.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+import Shared
+
+class EventLoggerDelegate: NSObject, URLSessionDelegate, URLSessionTaskDelegate, URLSessionDataDelegate {
+  func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+    if let error = error {
+      print("🛰 NETWORK Request Error: \(error.localizedDescription)")
+    } else {
+      print("🛰 NETWORK Request Completed Successfully")
+    }
+
+    if let request = task.originalRequest {
+      print("🛰 NETWORK Request LOG")
+      print(request.description)
+      print("URL: " + (request.url?.absoluteString ?? ""))
+      print("Method: " + (request.httpMethod ?? ""))
+      if let body = request.httpBody {
+        print("Body: " + (body.toPrettyPrintedString ?? ""))
+      }
+    }
+
+    if let response = task.response as? HTTPURLResponse {
+      print("🛰 NETWORK Response LOG")
+      print("StatusCode: \(response.statusCode)")
+      if let data = try? Data(contentsOf: response.url!) {
+        print("Data: \(data.toPrettyPrintedString ?? "")")
+      }
+    }
+  }
+}
