61ab5e4868b5ab8ed0f74980279b88c076bdd8c5
devminseok <qbq5000@gmail.com>
2024-08-04 02:36:54 +0900

    FEAT: 데이터베이스 초기화 코드 추가

--- Projects/Domain/AppService/Sources/AppService.swift
+++ Projects/Domain/AppService/Sources/AppService.swift
@@ -1,5 +1,34 @@
-import Foundation
+//
+//  AppService.swift
+//  AppService
+//
+//  Created by devMinseok on 8/4/24.
+//  Copyright © 2024 PomoNyang. All rights reserved.
+//
 
-public struct AppService {
-  public init() {}
+import Logger
+import DatabaseClientInterface
+
+import RealmSwift
+
+let currentSchemaVersion: UInt64 = 1
+
+public func initilizeDatabaseSystem(
+  databaseClient: DatabaseClient
+) async throws -> Void {
+  // DB 마이그레이션 정의
+  let configuration = Realm.Configuration(
+    schemaVersion: currentSchemaVersion
+  ) { migration, oldSchemaVersion in
+    Logger.shared.log(
+      category: .database,
+      "[Realm DB Migration] oldVersion: \(oldSchemaVersion) -> newVersion: \(currentSchemaVersion)"
+    )
+    
+    Logger.shared.log(category: .database, "[Realm DB Migration] Complete")
+  }
+  
+  Logger.shared.log(category: .database, "💾 Realm Path :\n\(configuration.fileURL?.absoluteString ?? "NONE")")
+  
+  try await databaseClient.initialize(configuration)
 }
