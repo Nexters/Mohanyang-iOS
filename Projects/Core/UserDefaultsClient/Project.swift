0750b4e497a67c0353e0eb0cdbcea52066ff2796
devminseok <qbq5000@gmail.com>
2024-08-04 18:38:24 +0900

    UserDefaultsClient 모듈 및 기능 추가

---
+++ Projects/Core/UserDefaultsClient/Project.swift
@@ -0,0 +1,29 @@
+//
+//  UserDefaultsClientTesting.swift
+//  UserDefaultsClientManifests
+//
+//  Created by devMinseok on 8/4/24.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+@_spi(Core)
+@_spi(Shared)
+import DependencyPlugin
+
+let project: Project = .makeTMABasedProject(
+  module: Core.UserDefaultsClient,
+  scripts: [],
+  targets: [
+    .sources,
+    .interface,
+    .tests,
+    .testing
+  ],
+  dependencies: [
+    .interface: [
+      .dependency(rootModule: Shared.self)
+    ]
+  ]
+)
