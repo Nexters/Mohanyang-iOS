9fa2f3d469d361901da301c213a3ef8507049bce
devminseok <qbq5000@gmail.com>
2024-08-16 11:39:58 +0900

    add: FeedbackGeneratorClient 모듈 추가

---
+++ Projects/Core/FeedbackGeneratorClient/Example/Sources/FeedbackGeneratorClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedbackGeneratorClientApp.swift
+//  FeedbackGeneratorClient
+//
+//  Created by devMinseok on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct FeedbackGeneratorClientApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
