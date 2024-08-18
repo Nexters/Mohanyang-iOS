8c8fc6ea5a0bfe254179f771f64360e65c155d15
devminseok <qbq5000@gmail.com>
2024-08-18 20:23:00 +0900

    refactor: NavigationStack을 AppView에 위치하도록 변경

--- Projects/Feature/Feature/Sources/AppView.swift
+++ Projects/Feature/Feature/Sources/AppView.swift
@@ -26,12 +26,20 @@ public struct AppView: View {
     Group {
       if let splashStore = store.scope(state: \.splash, action: \.splash) {
         SplashView(store: splashStore)
+          .zIndex(0)
       } else if let onboardingStore = store.scope(state: \.onboarding, action: \.onboarding) {
-        OnboardingView(store: onboardingStore)
+        NavigationStack {
+          OnboardingView(store: onboardingStore)
+            .zIndex(1)
+        }
       } else if let homeStore = store.scope(state: \.home, action: \.home) {
-        HomeView(store: homeStore)
+        NavigationStack {
+          HomeView(store: homeStore)
+            .zIndex(2)
+        }
       } else {
         Global.Color.black // MARK: - DB정보 없고 오프라인일때 Dialog 띄우기
+          .zIndex(3)
       }
     }
     .transition(.opacity)
