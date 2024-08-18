8923a325718af80a7c4487af415065d9e8f1b55b
devminseok <qbq5000@gmail.com>
2024-08-18 21:02:05 +0900

    refactor: 추상레이어를 import하지 않는 방향으로 변경

--- Projects/Core/Core/Sources/Exports.swift
+++ Projects/Core/Core/Sources/Exports.swift
@@ -5,14 +5,3 @@
 //  Created by devMinseok on 7/20/24.
 //  Copyright © 2024 PomoNyang. All rights reserved.
 //
-
-@_exported import APIClient
-@_exported import APIClientInterface
-@_exported import KeychainClient
-@_exported import KeychainClientInterface
-@_exported import UserDefaultsClient
-@_exported import UserDefaultsClientInterface
-@_exported import UserNotificationClient
-@_exported import UserNotificationClientInterface
-@_exported import DatabaseClient
-@_exported import DatabaseClientInterface
--- Projects/Shared/Shared/Sources/Exports.swift
+++ Projects/Shared/Shared/Sources/Exports.swift
@@ -5,11 +5,3 @@
 //  Created by devMinseok on 7/20/24.
 //  Copyright © 2024 PomoNyang. All rights reserved.
 //
-
-@_exported import Logger
-@_exported import DesignSystem
-@_exported import Utils
-
-@_exported import ThirdParty_SPM
-@_exported import ThirdParty_Firebase
-@_exported import ThirdParty_Realm
