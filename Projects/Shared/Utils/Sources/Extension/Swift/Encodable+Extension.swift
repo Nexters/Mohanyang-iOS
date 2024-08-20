2bc3b00b5990deb5d5398d36d03eb1e006916c34
devminseok <qbq5000@gmail.com>
2024-08-21 02:12:01 +0900

    refactor: Date를 위한 커스텀 인코더

--- Projects/Shared/Utils/Sources/Extension/Swift/Encodable+Extension.swift
+++ Projects/Shared/Utils/Sources/Extension/Swift/Encodable+Extension.swift
@@ -10,9 +10,41 @@ import Foundation
 
 extension Encodable {
   public func toDictionary() throws -> [String: Any] {
-    guard let data = try? JSONEncoder().encode(self),
+    guard let data = try? customEncoder.encode(self),
           let jsonData = try? JSONSerialization.jsonObject(with: data),
           let dictionaryData = jsonData as? [String: Any] else { return [:] }
     return dictionaryData
+  }
+  
+  private var customEncoder: JSONEncoder {
+    let encoder = JSONEncoder()
+    encoder.dateEncodingStrategy = .custom { date, encoder in
+      var container = encoder.singleValueContainer()
+      let formatter = ISO8601DateFormatter()
+      
+      // 2023-07-26T00:00:00Z
+      formatter.formatOptions = [.withInternetDateTime, .withColonSeparatorInTime]
+      if let dateString = formatter.string(for: date) {
+        try container.encode(dateString)
+        return
+      }
+      
+      // 2023-07-26T00:00:00.000Z
+      formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+      if let dateString = formatter.string(for: date) {
+        try container.encode(dateString)
+        return
+      }
+      
+      // 2023-07-26T00:00:00
+      formatter.formatOptions = [.withFullDate, .withTime, .withColonSeparatorInTime]
+      if let dateString = formatter.string(for: date) {
+        try container.encode(dateString)
+        return
+      }
+      
+      throw EncodingError.invalidValue(date, EncodingError.Context(codingPath: encoder.codingPath, debugDescription: "⚠️ Date encode 실패"))
+    }
+    return encoder
   }
 }
