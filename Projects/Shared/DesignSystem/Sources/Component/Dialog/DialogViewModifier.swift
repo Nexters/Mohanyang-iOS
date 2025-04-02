346e67687dbe9e9547f7364be6d71eed189312c3
devminseok <qbq5000@gmail.com>
2025-04-02 20:45:12 +0900

    Dialog 노출 방식 변경
    최상단 window에 띄우는걸로

--- Projects/Shared/DesignSystem/Sources/Component/Dialog/DialogViewModifier.swift
+++ Projects/Shared/DesignSystem/Sources/Component/Dialog/DialogViewModifier.swift
@@ -8,104 +8,120 @@
 
 import SwiftUI
 
-struct DialogViewModifier<T: Dialog>: ViewModifier {
-  @Binding var dialog: T?
-  
-  func body(content: Content) -> some View {
+struct DialogView<T: Dialog>: View {
+  let dialog: T
+  var onDismiss: (() -> Void)?
+
+  var body: some View {
     ZStack(alignment: .center) {
-      content
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .zIndex(1)
-      
-      if let dialog {
-        Global.Color.black.opacity(Global.Opacity._50d)
-          .ignoresSafeArea()
-          .zIndex(2)
-        
-        // MARK: Title & SubTitle
-        VStack(spacing: Alias.Spacing.large) {
-          VStack(spacing: .zero) {
-            HStack(alignment: .center, spacing: 8) {
-              Text(dialog.title)
-                .font(Typography.header4)
-                .foregroundStyle(Alias.Color.Text.primary)
-                .lineLimit(1)
-                .frame(maxWidth: .infinity, alignment: .leading)
-              
-              if dialog.showCloseButton {
-                Button(icon: DesignSystemAsset.Image._24ClosePrimary.swiftUIImage) {
-                  self.dialog = nil
-                }
-                .buttonStyle(.icon(isFilled: false, level: .primary))
+      Global.Color.black.opacity(Global.Opacity._50d)
+        .ignoresSafeArea()
+        .transition(.opacity)
+
+      VStack(spacing: Alias.Spacing.large) {
+        // MARK: Title & SubTitle 영역
+        VStack(spacing: 0) {
+          HStack(alignment: .center, spacing: 8) {
+            Text(dialog.title)
+              .font(Typography.header4)
+              .foregroundStyle(Alias.Color.Text.primary)
+              .lineLimit(1)
+              .frame(maxWidth: .infinity, alignment: .leading)
+
+            if dialog.showCloseButton {
+              Button {
+                onDismiss?()
+              } label: {
+                DesignSystemAsset.Image._24ClosePrimary.swiftUIImage
               }
-            }
-            .frame(minHeight: 40)
-            if let subTitle = dialog.subTitle {
-              Text(subTitle)
-                .font(Typography.subBodyR)
-                .foregroundStyle(Alias.Color.Text.secondary)
-                .lineLimit(4)
-                .frame(maxWidth: .infinity, alignment: .leading)
+              .buttonStyle(.icon(isFilled: false, level: .primary))
             }
           }
+          .frame(minHeight: 40)
 
-          // MARK: Buttons
-          HStack(spacing: Alias.Spacing.medium) {
-            Button(
-              title: LocalizedStringKey(dialog.firstButton.title),
-              leftIcon: { dialog.firstButton.leftIcon },
-              rightIcon: { dialog.firstButton.rightIcon },
-              action: {
-                self.dialog = nil
-                Task {
-                  await dialog.firstButton.action?()
-                }
-              }
+          if let subTitle = dialog.subTitle {
+            Text(subTitle)
+              .font(Typography.subBodyR)
+              .foregroundStyle(Alias.Color.Text.secondary)
+              .lineLimit(4)
+              .frame(maxWidth: .infinity, alignment: .leading)
+          }
+        }
+
+        // MARK: 버튼 영역
+        HStack(spacing: Alias.Spacing.medium) {
+          Button {
+            Task {
+              await dialog.firstButton.action?()
+            }
+            onDismiss?()
+          } label: {
+            Text(LocalizedStringKey(dialog.firstButton.title))
+          }
+          .buttonStyle(
+            .box(
+              level: dialog.firstButton.action == nil ? .tertiary : .primary,
+              size: .medium,
+              width: .low
             )
+          )
+
+          if let secondButton = dialog.secondButton {
+            Button {
+              Task {
+                await secondButton.action?()
+              }
+              onDismiss?()
+            } label: {
+              Text(LocalizedStringKey(secondButton.title))
+            }
             .buttonStyle(
               .box(
-                level: dialog.firstButton.action == nil ? .tertiary : .primary,
+                level: secondButton.action == nil ? .tertiary : .primary,
                 size: .medium,
                 width: .low
               )
             )
-            if let secondButton = dialog.secondButton {
-              Button(
-                title: LocalizedStringKey(secondButton.title),
-                leftIcon: { secondButton.leftIcon },
-                rightIcon: { secondButton.rightIcon },
-                action: {
-                  self.dialog = nil
-                  Task {
-                    await secondButton.action?()
-                  }
-                }
-              )
-              .buttonStyle(
-                .box(
-                  level: secondButton.action == nil ? .tertiary : .primary,
-                  size: .medium,
-                  width: .low
-                )
-              )
-            }
           }
         }
-        .padding(.all, Alias.Spacing.xLarge)
-        .background(Global.Color.white)
-        .cornerRadius(Alias.BorderRadius.medium)
-        .padding(.horizontal, Alias.Spacing.xLarge)
-        .zIndex(3)
       }
+      .padding(Alias.Spacing.xLarge)
+      .background(Global.Color.white)
+      .cornerRadius(Alias.BorderRadius.medium)
+      .padding(.horizontal, Alias.Spacing.xLarge)
+      .transition(.scale)
     }
-    .animation(.easeInOut(duration: 0.3), value: self.dialog == nil)
   }
 }
 
 extension View {
-  public func dialog<T: Dialog>(
-    dialog: Binding<T?>
-  ) -> some View {
-    return self.modifier(DialogViewModifier(dialog: dialog))
+  public func dialog<T: Dialog>(dialog: Binding<T?>) -> some View {
+    self
+      .onChange(of: dialog.wrappedValue) { _, newValue in
+        withAnimation {
+          if let newDialog = newValue {
+            let view = DialogView(
+              dialog: newDialog,
+              onDismiss: {
+                dialog.wrappedValue = nil
+              }
+            )
+            OverlayManager.shared.showOverlay(view)
+          } else {
+            OverlayManager.shared.hideOverlay()
+          }
+        }
+      }
+      .onAppear {
+        if let newDialog = dialog.wrappedValue {
+          let view = DialogView(
+            dialog: newDialog,
+            onDismiss: {
+              dialog.wrappedValue = nil
+            }
+          )
+          OverlayManager.shared.showOverlay(view)
+        }
+      }
   }
 }
